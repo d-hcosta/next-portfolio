dee9186c64025d0764b8bfa1f1c5fefdf12ee002
d-hcosta <d.hcosta@proton.me>
2023-08-28 13:25:13 -0300

    fix: FIxing tailwind extend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -12,7 +12,7 @@ const config: Config = {
         container: "1440px",
         contentContainer: "1140px",
         containerSmall: "1024px",
-        containerxs: "768px"
+        containerxs: "768px",
       },
       screens: {
         xs: "320px",
@@ -26,18 +26,15 @@ const config: Config = {
       },
       fontFamily: {
         bodyFont: ["Montserrat", "sans-serif"],
-        titleFont: ["Inter", "sans-serif"]
-      },
-      boxShadow: {
-        navbarShadow: "0 10px 30px -10px rgba(2, 12, 27, 0.7)"
+        titleFont: ["Inter", "sans-serif"],
       },
       colors: {
         bodyColor: "#0A192F",
         textGreen: "#64ffda",
         textLight: "#ccd6f6",
         textDark: "#8892b0",
-        hoverColor: "#rgba(100, 255, 218, 0.1)"
-      }
+        hoverColor: "rgba(100, 255, 218, 0.1)",
+      },
     },
   },
   plugins: [],
