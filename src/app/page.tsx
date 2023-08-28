6483b76813cb8ad2915d68242e9d7a60ec896368
d-hcosta <d.hcosta@proton.me>
2023-08-28 13:25:59 -0300

    feat: Implementing new components to main file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import { Navbar } from "@/components";
+import { Navbar } from "@/components"
+import { AsideLeft } from "@/components/AsideLeft/AsideLeft"
+import { AsideRight } from "@/components/AsideRight"
 
 export default function Home() {
   return (
-  <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
-    <Navbar />
-  </main>
+    <main className="scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60 h-screen w-full  overflow-x-hidden overflow-y-scroll bg-bodyColor font-bodyFont text-textLight">
+      <Navbar />
+
+      <div className="h-[88vh] w-full items-center justify-between gap-20 xl:flex">
+        <div className="fixed bottom-0 left-0 hidden h-full w-32 xl:inline-flex">
+          <AsideLeft />
+        </div>
+
+        <div className="mx-auto h-[88vh] p-4">MIddle</div>
+
+        <div className="fixed bottom-0 right-0 hidden h-full w-32 xl:inline-flex">
+          <AsideRight />
+        </div>
+      </div>
+    </main>
   )
 }
