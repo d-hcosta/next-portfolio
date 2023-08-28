a907c0e34348bea27a56df52880c7d8001ba5e9d
d-hcosta <d.hcosta@proton.me>
2023-08-28 13:26:47 -0300

    feat: Creating AsideRight component

---
+++ src/components/AsideRight/AsideRight.tsx
@@ -0,0 +1,11 @@
+export function AsideRight() {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-end gap-6 text-textLight">
+      <a href="mailto:d.hcosta@proton.me">
+        <p className="w-72 rotate-90 text-sm tracking-wide text-textGreen">d.hcosta@proton.me</p>
+      </a>
+
+      <span className="inline-flex h-32 w-[2px] bg-textDark"></span>
+    </div>
+  )
+}
