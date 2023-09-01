5a5b17088289ffbc6d7b9c6a7acee39a85a30a7a
d-hcosta <d.hcosta@proton.me>
2023-09-01 00:26:10 -0300

    fix: Fixing AsideLeft component

--- src/components/AsideLeft/AsideLeft.tsx
+++ src/components/AsideLeft/AsideLeft.tsx
@@ -1,24 +1,34 @@
+"use client"
+
 import { TbBrandGithub } from "react-icons/tb"
 import { SlSocialLinkedin } from "react-icons/sl"
+import { motion } from "framer-motion"
 
 export function AsideLeft() {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-end gap-4 text-textLight">
-      <div className="flex flex-col gap-4 ">
-        <a href="https://github.com/d-hcosta" target="_blank">
-          <span className="inline-flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-hoverColor text-xl transition-all duration-300 hover:-translate-y-2 hover:text-textGreen">
-            <TbBrandGithub />
-          </span>
-        </a>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1.5 }}
+      className="fixed bottom-0 left-0 hidden h-full w-32 xl:inline-flex"
+    >
+      <div className="flex h-full w-full flex-col items-center justify-end gap-4 text-textLight">
+        <div className="flex flex-col gap-4 ">
+          <a href="https://github.com/d-hcosta" target="_blank">
+            <span className="inline-flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-hoverColor text-xl transition-all duration-300 hover:-translate-y-2 hover:text-textGreen">
+              <TbBrandGithub />
+            </span>
+          </a>
 
-        <a href="https://www.linkedin.com/in/diegohoc/" target="_blank">
-          <span className="inline-flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-hoverColor text-xl transition-all duration-300 hover:-translate-y-2 hover:text-textGreen">
-            <SlSocialLinkedin />
-          </span>
-        </a>
+          <a href="https://www.linkedin.com/in/diegohoc/" target="_blank">
+            <span className="inline-flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-hoverColor text-xl transition-all duration-300 hover:-translate-y-2 hover:text-textGreen">
+              <SlSocialLinkedin />
+            </span>
+          </a>
+        </div>
+
+        <div className="h-32 w-[2px] bg-textDark "></div>
       </div>
-
-      <div className="h-32 w-[2px] bg-textDark "></div>
-    </div>
+    </motion.div>
   )
 }
