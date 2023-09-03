92f90cce462f588ec9520e8aec663b4f5ee9d1b3
d-hcosta <d.hcosta@proton.me>
2023-09-03 17:56:36 -0300

    feat: Implementing Contact component

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,14 @@
+export function Contact() {
+  return (
+    <section
+      id="contact"
+      className="mx-auto flex max-w-contentContainer flex-col items-center justify-center gap-4 py-10 xl:py-32"
+    >
+      <p className="txt-lg flex items-center font-titleFont font-semibold tracking-wide text-textGreen">
+        04. What's next?
+      </p>
+
+      <h2 className="font-titleFont text-5xl font-semibold">Get In Touch</h2>
+    </section>
+  )
+}
