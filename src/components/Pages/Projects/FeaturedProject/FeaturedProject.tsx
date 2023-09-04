2524c7eddc688144b2bba119a7903df2cca33d92
d-hcosta <d.hcosta@proton.me>
2023-09-04 10:30:37 -0300

    feat: Implementing FeaturedProject

--- src/components/Pages/Projects/FeaturedProject/FeaturedProject.tsx
+++ src/components/Pages/Projects/FeaturedProject/FeaturedProject.tsx
@@ -11,6 +11,7 @@ export function FeaturedProject({
   imgSrc,
   techArray,
   reverse,
+  openDetailedProject,
 }: FeaturedProjectProps) {
   const ref = useRef(null)
   const isInView = useInView(ref, { once: true })
@@ -24,10 +25,9 @@ export function FeaturedProject({
 
   return (
     <div className={`flex flex-col gap-6 ${reverse ? "xl:flex-row-reverse" : "xl:flex-row"}`}>
-      <a
-        href="https://www.elorocket.com/"
-        target="_blank"
-        className="group relative h-auto w-full xl:w-1/2"
+      <div
+        className="group relative h-auto w-full cursor-pointer xl:w-1/2"
+        onClick={() => openDetailedProject()}
       >
         <div>
           <Reveal>
@@ -36,7 +36,7 @@ export function FeaturedProject({
 
           <div className="absolute left-0 top-0 h-full w-full rounded-lg bg-black/30 duration-300 group-hover:bg-transparent"></div>
         </div>
-      </a>
+      </div>
 
       <div className="z-10 flex w-full flex-col items-end gap-6 text-right lgl:justify-between xl:-ml-16 xl:w-1/2">
         <div>
@@ -49,13 +49,18 @@ export function FeaturedProject({
           </Reveal>
         </div>
 
-        <p
+        <motion.p
+          ref={ref}
+          variants={{ hidden: { opacity: 0, y: 10 }, visible: { opacity: 1, y: 0 } }}
+          initial="hidden"
+          animate={techControls}
+          transition={{ duration: 0.5, delay: 0.65 }}
           className={`rounded-md bg-[#232323] p-2 text-sm md:p-6 md:text-base ${
             reverse && "xl:-mr-16"
           }`}
         >
           {projectShortDescription}
-        </p>
+        </motion.p>
 
         <ul className="flex justify-between gap-2 font-titleFont text-xs tracking-wide text-textDark md:gap-5 md:text-sm">
           {techArray?.map((tech, index) => (
@@ -73,13 +78,17 @@ export function FeaturedProject({
         </ul>
 
         <div className="flex gap-4 text-2xl">
-          <a
-            href="http://elorocket.com"
-            target="_blank"
-            className="duration-300 hover:text-textGreen"
+          <motion.div
+            ref={ref}
+            variants={{ hidden: { opacity: 0, y: 10 }, visible: { opacity: 1, y: 0 } }}
+            initial="hidden"
+            animate={techControls}
+            transition={{ duration: 0.5, delay: 0.25 }}
+            className="flex cursor-pointer gap-2 font-bodyFont font-semibold text-textGreen duration-300 hover:text-textGreen/75"
+            onClick={() => openDetailedProject()}
           >
-            <RxOpenInNewWindow />
-          </a>
+            <RxOpenInNewWindow /> See more
+          </motion.div>
         </div>
       </div>
     </div>
