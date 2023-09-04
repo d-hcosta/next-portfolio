e46a893272158e3ebf0c4e3a2972c6ac35ef46a6
d-hcosta <d.hcosta@proton.me>
2023-09-04 05:41:25 -0300

    feat: Creating constants to the Experiences component

---
+++ src/constants/Experience/ExperienceConstants.ts
@@ -0,0 +1,38 @@
+export const ExperiencesConstants = [
+  {
+    name: "SignoWeb",
+    projectUrl: "https://site.signoweb.com.br/",
+    projectName: "SignoWeb",
+    periodInTheProject: "July 6, 2022 - October 10, 2022",
+    arrayOfDescriptions: [
+      "Continuous improvement of existing systems with the implementation of new features and bug fixes.",
+      "Maintenance on existing projects and monolithic application development using the MERN Stack.",
+      "Implementation of new functionalities and maintenance in diversified systems.",
+    ],
+    positionHeld: "Fullstack Developer",
+  },
+  {
+    name: "EloRocket",
+    projectUrl: "https://www.elorocket.com/",
+    projectName: "EloRocket",
+    periodInTheProject: "May 13, 2022 - August 2, 2023",
+    arrayOfDescriptions: [
+      "Active participation in the software architecture and construction of the entire flow",
+      "Development of highly functional and adaptable systems to meet the needs of the company.",
+      "Development of a complete administrative dashboard for e-commerce management using React, Typescript, Node and PostgreSQL",
+    ],
+    positionHeld: "Software Developer",
+  },
+  {
+    name: "Freelancer",
+    projectUrl: "#",
+    projectName: "Freelancer",
+    periodInTheProject: "Jan 23, 2022 - Present",
+    arrayOfDescriptions: [
+      "Successful development of e-commerce and landing pages in React with Styled Components/Tailwind.",
+      "Strengthening skills in agile software development and fullstack.",
+      "Strengthening machine learning with Python.",
+    ],
+    positionHeld: "Fullstack Developer",
+  },
+]
