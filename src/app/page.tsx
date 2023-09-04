b43b950d07db28d3dc0079b74237c6bbe2c12d85
d-hcosta <d.hcosta@proton.me>
2023-09-04 10:37:41 -0300

    fix: Implementing background particles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       </div>
 
       <div className="fixed bottom-0 left-0 right-0 top-0 -z-10 bg-bodyColor">
-        {/* <BackgroundParticles /> */}
+        <BackgroundParticles />
       </div>
     </main>
   )
