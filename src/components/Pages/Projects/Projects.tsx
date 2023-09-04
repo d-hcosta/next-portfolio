07e0eb7a4037c7ac13049a89fb0ac2e59c139b67
d-hcosta <d.hcosta@proton.me>
2023-09-04 10:30:30 -0300

    feat: Implementing project component

--- src/components/Pages/Projects/Projects.tsx
+++ src/components/Pages/Projects/Projects.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
 import { SectionTitle } from "@/components"
 import { FeaturedProject } from "./FeaturedProject"
 import { ProjectsConstants } from "@/constants"
+import { ProjectModal } from "./ProjectModal"
 
 export function Projects() {
+  const [openedProject, setOpenedProject] = useState<ProjectProps | null>(null)
+  const [isOpen, setIsOpen] = useState(false)
+
+  function handleOpenDetailedProjectModal(project: ProjectProps) {
+    setIsOpen(!isOpen)
+    setOpenedProject(project)
+  }
+
   return (
     <section id="projects" className="mx-auto max-w-container py-24 lgl:px-20">
       <SectionTitle number="03" title="Some things I built" />
@@ -14,13 +24,15 @@ export function Projects() {
               key={`project-${index}`}
               imgSrc={project.image}
               projectName={project.name}
-              projectShortDescription={project.description}
+              projectShortDescription={project.shortDescription}
               techArray={project.techArray}
               reverse={project.reverse}
+              openDetailedProject={() => handleOpenDetailedProjectModal(project)}
             />
           ))}
         </div>
       </div>
+      <ProjectModal isOpen={isOpen} setIsOpen={setIsOpen} project={openedProject} />
     </section>
   )
 }
