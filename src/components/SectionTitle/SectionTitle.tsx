b80821cca5a8f5238ae366c48f4a4d8c56d2958e
d-hcosta <d.hcosta@proton.me>
2023-09-04 05:41:37 -0300

    feat: Implementing animations to the section title

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useRef } from "react"
+import { motion, useAnimation, useInView } from "framer-motion"
+
 export function SectionTitle({ number, title }: SectionTitleProps) {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
+  const titleControl = useAnimation()
+
+  useEffect(() => {
+    if (isInView) {
+      titleControl.start("visible")
+    }
+  }, [isInView, titleControl])
+
   return (
-    <h2 className="flex items-center font-titleFont text-2xl font-semibold">
+    <motion.h2
+      ref={ref}
+      variants={{ hidden: { opacity: 0, y: 10 }, visible: { opacity: 1, y: 0 } }}
+      initial="hidden"
+      animate={titleControl}
+      transition={{ duration: 0.5, delay: 0.25 }}
+      className="flex items-center font-titleFont text-2xl font-semibold"
+    >
       <span className="mr-2 text-base text-textGreen md:text-lg">{number}.</span>
       {title}
-      <span className="bg-gray700 ml-6 hidden h-[.5px] md:inline-flex md:w-60 lgl:w-72"></span>
-    </h2>
+      <span className="ml-6 h-px w-full bg-gray-500 bg-opacity-40 md:inline-flex md:w-60 lgl:w-72"></span>
+    </motion.h2>
   )
 }
