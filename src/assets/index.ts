a5b515da380190102322295e5e5ce89954c97029
d-hcosta <d.hcosta@proton.me>
2023-09-07 22:47:34 -0300

    feat: Creating new index files

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,7 @@ import characterAbout from "./images/character-about.png"
 import eloRocket from "./images/elorocket.png"
 import portfolioV1 from "./images/dhcosta.png"
 import pomodoro from "./images/pomodoro.png"
+import brazilSvg from "./svg/brazil.svg"
+import usaSvg from "./svg/usa.svg"
 
-export { character, characterAbout, eloRocket, portfolioV1, pomodoro }
+export { character, characterAbout, eloRocket, portfolioV1, pomodoro, brazilSvg, usaSvg }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export * from "./Projects"
 export * from "./Experience"
 export * from "./Navbar"
+export * from "./Language"
