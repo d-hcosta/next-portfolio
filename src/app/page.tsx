ab819139b7f95bcb750c6c2e0e8bf57da1b0d9b6
d-hcosta <d.hcosta@proton.me>
2023-09-07 22:48:02 -0300

    feat: Implementing language provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { LanguageProvider } from "@/hooks"
+
 import {
   About,
   AsideLeft,
@@ -14,23 +16,25 @@ import {
 export default function Home() {
   return (
     <main className="h-screen w-full overflow-x-hidden overflow-y-scroll bg-bodyColor font-bodyFont text-textLight scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
-      <div className="z-50">
-        <Navbars />
-      </div>
-
-      <div className="h-[88vh] w-full items-center justify-between gap-20 xl:flex">
-        <AsideLeft />
-
-        <div className="mx-auto h-[88vh] w-full p-4">
-          <HeroSection />
-          <About />
-          <Experience />
-          <Projects />
-          <Contact />
+      <LanguageProvider>
+        <div className="z-50">
+          <Navbars />
         </div>
 
-        <AsideRight />
-      </div>
+        <div className="h-[88vh] w-full items-center justify-between gap-20 xl:flex">
+          <AsideLeft />
+
+          <div className="mx-auto h-[88vh] w-full p-4">
+            <HeroSection />
+            <About />
+            <Experience />
+            <Projects />
+            <Contact />
+          </div>
+
+          <AsideRight />
+        </div>
+      </LanguageProvider>
     </main>
   )
 }
