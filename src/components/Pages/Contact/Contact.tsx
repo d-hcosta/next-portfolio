e71704d570c8cf6a40bde7b0a475c10b52730f63
d-hcosta <d.hcosta@proton.me>
2023-09-18 08:57:56 -0300

    feat: Adding animation to the contact section

--- src/components/Pages/Contact/Contact.tsx
+++ src/components/Pages/Contact/Contact.tsx
@@ -1,3 +1,4 @@
+import { Reveal } from "@/components"
 import { MdEmail } from "react-icons/md"
 
 export function Contact() {
@@ -6,32 +7,42 @@ export function Contact() {
       id="contact"
       className="mx-auto flex max-w-contentContainer flex-col items-center justify-center gap-4 py-10 xl:py-32"
     >
-      <p className="txt-lg flex items-center font-titleFont font-semibold tracking-wide text-textGreen">
-        04. What's next?
-      </p>
+      <Reveal>
+        <p className="txt-lg flex items-center font-titleFont font-semibold tracking-wide text-textGreen">
+          04. What's next?
+        </p>
+      </Reveal>
 
-      <h1 className="font-titleFont text-7xl font-black">
-        Contact<span className="text-textGreen">.</span>
-      </h1>
+      <Reveal>
+        <h1 className="font-titleFont text-7xl font-black">
+          Contact<span className="text-textGreen">.</span>
+        </h1>
+      </Reveal>
 
       <div className="w-2/3 text-center text-base tracking-wider">
-        <p>
-          Please feel free to contact me via email if you would like to establish a connection.
-          Also, I'm available on{" "}
-          <span className="text-textGreen">
-            <a href="https://www.linkedin.com/in/diegohoc/" target="_blank">
-              LinkedIn
-            </a>
-          </span>{" "}
-          if you prefer a more agile approach to communication.
-        </p>
+        <Reveal>
+          <p>
+            Please feel free to contact me via email if you would like to establish a connection.
+            Also, I'm available on{" "}
+            <span className="text-textGreen">
+              <a href="https://www.linkedin.com/in/diegohoc/" target="_blank">
+                LinkedIn
+              </a>
+            </span>{" "}
+            if you prefer a more agile approach to communication.
+          </p>
+        </Reveal>
 
         <div className="mt-5 flex items-center justify-center gap-3 text-2xl font-semibold hover:text-textGreen ">
-          <MdEmail />
+          <Reveal>
+            <MdEmail />
+          </Reveal>
 
-          <a href="mailto:d.hcosta@proton.me" target="_blank">
-            d.hcosta@proton.me
-          </a>
+          <Reveal>
+            <a href="mailto:d.hcosta@proton.me" target="_blank">
+              d.hcosta@proton.me
+            </a>
+          </Reveal>
         </div>
       </div>
     </section>
