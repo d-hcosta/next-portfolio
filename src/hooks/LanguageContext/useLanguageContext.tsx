6798b3a342965574c06baaf73d9bdc32652ad174
d-hcosta <d.hcosta@proton.me>
2023-09-20 12:51:48 -0300

    feat: Implementing translate function in Language hook to mark text

--- src/hooks/LanguageContext/useLanguageContext.tsx
+++ src/hooks/LanguageContext/useLanguageContext.tsx
@@ -5,11 +5,44 @@ const LanguageContext = createContext<LanguageContextProps>({} as LanguageContex
 import { languages } from "@/constants"
 
 export function LanguageProvider({ children }: LanguageProviderProps) {
+  const [isLanguageModalOpen, setIsLanguageModalOpen] = useState<boolean>(false)
   const [languageCode, setLanguageCode] = useState<keyof TLanguage>("BR")
 
   const translate = (key: TLanguageKey) => {
-    const getLanguage = languages[key][languageCode]
-    return getLanguage
+    const translation = languages[key][languageCode]
+    if (translation) {
+      // Check if translation is an array of strings
+      if (Array.isArray(translation)) {
+        return translation.map((text, index) => (
+          <span key={index} className="text-textGreen">
+            {text}
+          </span>
+        ))
+      }
+
+      // Split the translation string into parts using '{{' and '}}' as delimiters
+      const parts = translation.split(/({{.*?}})/g)
+
+      // Map each part to JSX elements for spans or plain text
+      const translatedText = parts.map((part, index) => {
+        if (part.startsWith("{{") && part.endsWith("}}")) {
+          // Remove '{{' and '}}' and use the inner content as the span text
+          const spanText = part.substring(2, part.length - 2)
+          return (
+            <span key={index} className="text-textGreen">
+              {spanText}
+            </span>
+          )
+        } else {
+          return part
+        }
+      })
+
+      return <>{translatedText}</>
+    }
+
+    // Fallback to English or return an empty string if no translation is found
+    return languages[key]["EN"] || ""
   }
 
   const handleChangeLanguage = (language: keyof TLanguage) => {
@@ -17,12 +50,22 @@ export function LanguageProvider({ children }: LanguageProviderProps) {
     localStorage.setItem("selectedLang", language)
   }
 
+  function handleOpenLanguageModal() {
+    setIsLanguageModalOpen(!isLanguageModalOpen)
+  }
+
   useEffect(() => {
     const storageLang = localStorage.getItem("selectedLang")
     if (storageLang !== null) handleChangeLanguage(storageLang as keyof TLanguage)
   }, [languageCode])
 
-  const provide = { languageCode, translate, handleChangeLanguage }
+  const provide = {
+    languageCode,
+    translate,
+    isLanguageModalOpen,
+    handleOpenLanguageModal,
+    handleChangeLanguage,
+  }
   return <LanguageContext.Provider value={provide}>{children}</LanguageContext.Provider>
 }
 
